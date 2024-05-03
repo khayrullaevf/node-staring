5ec621b534e94170f8de2a1341af0fed29967a45
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-05-03 12:49:08 +0300

    read and write file , server,routing ,sendinghtml as response

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const fs=require('fs')
+const http=require('http')
 
 
 
@@ -13,9 +14,65 @@ const fs=require('fs')
 
 
 
+let html=fs.readFileSync('./templates/index.html','utf-8')
+
+//Lecture 8. CREATE SERVER
+
+//1.CREATE SERVER
+const server=http.createServer((request,response)=>{
+    console.log('A new request has been recieved');
+    
+    let path=request.url
+  
+    if (path==='/'||path.toLowerCase()==='/home'){
+      response.end(html.replace('{{%CONTENT%}}','You are in home page'))
+    }else if (path.toLowerCase()==='/about') {
+        response.end(html.replace('{{%CONTENT%}}','You are in about page'))
+
+    }else if(path.toLowerCase()==='/contact'){
+        response.end(html.replace('{{%CONTENT%}}','You are in contact page'))
+    }else{
+        response.end(html.replace('{{%CONTENT%}}','PAGE NOT FOUND !'))
+    }
+})
 
 
-//Lecture 5.  How to read and write file 
+
+
+
+//2.START SERVER
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Server has started');
+})
+
+
+
+
+
+
+
+//Lecture 7.  How to read and write file asynchronously
+
+// fs.readFile('./files/input.text','utf-8',(error,data)=>{
+//       console.log(error);
+//       console.log(data);
+//       fs.readFile(`./files/${data}`,'utf-8',(error2,data2)=>{
+//         console.log(data2);
+//         fs.readFile(`./files/append.text`,'utf-8',(error3,data3)=>{
+//             console.log(data3);
+//                 let content=`Data read from input.xt : ${data2}.\nDate created ${new Date()}`
+//                 fs.writeFile('./files/output.txt',content,()=>{
+//                     console.log('File written successfully');
+//                 })
+//           })
+//       })
+// })
+
+//  console.log('Reading file....');
+
+
+//Lecture 5.  How to read and write file synchronously
 // let text=fs.readFileSync('./files/input.text','utf-8')
 // console.log(text);
 
