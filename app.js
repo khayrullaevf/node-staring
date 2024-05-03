5ea86aa0f0f18d0de8dc56d078f8c1e5d4c41980
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-05-03 09:14:44 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+const fs=require('fs')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Lecture 5.  How to read and write file 
+// let text=fs.readFileSync('./files/input.text','utf-8')
+// console.log(text);
+
+
+// let content=`Data read from input.xt : ${text}.\nDate created ${new Date()}`
+
+// fs.writeFileSync('./files/output.txt',content)
+
+
+//Lecture 4. How to output something  into terminal and how to read inputs from terminal
+
+// const readline=require('readline')
+
+
+// const rl=readline.createInterface({
+//     input:process.stdin,
+//     output:process.stdout
+// })
+
+
+
+// rl.question("Enter name  :  ",(name)=>{
+//     console.log('Hello '+name);
+//     rl.close()
+// })
+
+
+// rl.on('close',()=>{
+//     console.log('EInterface closed');
+//     process.exit(0)
+// })
